c26abafee5efbec71c2a0cb5c70c7e438cfc5f0b
KoldozMMA3 <128846559+KoldozMMA3@users.noreply.github.com>
2023-04-28 02:18:49 +0000

    Creamos la funcion de PREORDEN

--- student_file.c
+++ student_file.c
@@ -30,53 +30,13 @@ struct node* insert(struct node* raiz, int value) {
     return raiz;
 }
 
-   /**
-   Código
-    **/
-
-   //si el árbol está vacio
-   if(root == NULL) {
-           root = tempNode;
-           actual= root;
-           padre = root;
-           
-   } else {
-        actual = root;
-        padre = NULL;
-
-      while(1) {
-         padre = actual;
-
-         //recorrido por la izquierda del árbol
-         if(data < padre->value) {
-            actual=actual->left;
-
-            //insertamos en izquierda
-            if(actual == NULL) {
-               padre ->left = tempNode;
-               return; 
-            }
-         }
-         //recorrido por la derecha del árbol
-         else {
-             actual=actual->right;
-            //insertamos en derecha
-            if(actual == NULL) {
-               padre ->right = tempNode;
-               return; 
-            }
-         }
-      }
-   }
-}
-
-void inorder_recorrido(struct node* raiz) {
-   if(raiz != NULL) {
-      inorder_recorrido (raiz -> left);
-      printf("%i \n",raiz->value);
-      inorder_recorrido (raiz -> right);
-            
-  }
+// Recorrido Preorden
+void preorden_recorrido(struct node* raiz) {
+    if (raiz != NULL) {
+        printf("%i ", raiz->value);
+        preorden_recorrido(raiz->left);
+        preorden_recorrido(raiz->right);
+    }
 }
 
 int main() {
